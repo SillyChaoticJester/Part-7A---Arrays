ecc7d1a2eb6ad9dfb0b15faeaa5fca2ebe83c654
Miracle McMahon <339663114@GoTVDSB.ca>
2025-03-07 13:39:07 -0500

    Working on the assignment a little bit

--- Program.cs
+++ Program.cs
@@ -6,11 +6,27 @@
         {
             Random generator = new Random();
             int[] arrayTest = new int[30];
+            int count = 0;
             for (int i = 0; i < arrayTest.Length; i++)
             {
                 arrayTest[i] = generator.Next(1, 101);
                 Console.Write(arrayTest[i] + " ");
             }
+            for (int f = 0; f < arrayTest.Length; f++)
+            {
+                if (arrayTest[f] % 2 == 0)
+                {
+                    count++;
+                }
+                if (arrayTest[f] % 5 == 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(arrayTest[f]);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"There was {count} even numbes on in the list.");
         }
     }
 }
